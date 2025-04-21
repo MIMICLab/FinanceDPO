993754be846e447aaaafb8f5cbd4269cf6596d75
Taehoon Kim <jamesk1228@gmail.com>
2025-04-21 15:40:55 +0900

    use tensorboard

--- src/train.py
+++ src/train.py
@@ -1,16 +1,33 @@
 #!/usr/bin/env python
+"""Training entry point for DPO‑Finance with **TensorBoard** logging.
+
+Hydra config `dpo.yaml` controls all hyper‑parameters.  Enable/disable the
+TensorBoard logger via `logger.use_tb`.
+"""
+from __future__ import annotations
+
 import pytorch_lightning as pl
 import hydra
 from omegaconf import DictConfig, OmegaConf
-from pytorch_lightning.loggers import WandbLogger
+from pytorch_lightning.loggers import TensorBoardLogger
 
 from dpo_forecasting.models.dpo_model import DPOModel
 from dpo_forecasting.data.dataset import PreferenceDataModule
 
 
+def build_logger(cfg: DictConfig):
+    if not cfg.logger.use_tb:
+        return None
+    return TensorBoardLogger(
+        save_dir="tb_logs",
+        name=cfg.logger.experiment_name,
+        default_hp_metric=False,
+    )
+
+
 @hydra.main(config_path="dpo_forecasting/configs", config_name="dpo", version_base=None)
 def main(cfg: DictConfig) -> None:
-    # DataModule
+    # DataModule ---------------------------------------------------------
     dm = PreferenceDataModule(
         pairs_file=cfg.dataset.pairs_file,
         prices_dir=cfg.dataset.prices_dir,
@@ -20,21 +37,14 @@ def main(cfg: DictConfig) -> None:
         val_fraction=cfg.dataset.val_fraction,
     )
 
-    # Model
+    # Model --------------------------------------------------------------
     model = DPOModel(cfg)
 
-    # Logger (W&B optional)
-    logger = None
-    if cfg.logger.use_wandb:
-        logger = WandbLogger(
-            project=cfg.logger.project,
-            name=cfg.logger.run_name,
-            log_model="all",
-            config=OmegaConf.to_container(cfg, resolve=True),
-        )
+    # Logger -------------------------------------------------------------
+    tb_logger = build_logger(cfg)
 
-    # Trainer
-    trainer = pl.Trainer(logger=logger, **cfg.trainer)
+    # Trainer ------------------------------------------------------------
+    trainer = pl.Trainer(logger=tb_logger, **cfg.trainer)
     trainer.fit(model, datamodule=dm)
 
 
